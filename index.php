675fa144ccdd8ff556de922a7d606d25ddaf84e9
triplog <triplog@users.noreply.github.com>
2014-06-01 12:21:04 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>画像ジェネレータ</title>
+<meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="./../../../main.css">
+  <link rel="stylesheet" href="./../../pg.css">
+  <link rel="stylesheet" href="ra-men.css?0523">
+</head>
+<nav id="breadcrumbs">
+  <a href="./../../../">ホーム</a> &gt;
+  <a href="./../../">プログラミング</a> &gt;
+  <a href="./../">月1</a> &gt;
+</nav>
+
+<body>
+<h1>画像ジェネレータ</h1>
+<?php// echo file_get_contents("index.html"); ?>
+<h2 class="js">◆作成したもの</h2>
+<blockquote>
+  <a href="ra-men34.php"><img src="ra-men_thumb.png?0523" class="thumb" alt="ラーメン三銃士"></a>
+  <a href="masaru.php"><img src="masaru_thumb.png"  class="thumb" alt="マサル"></a>
+  <a href="magicring.php"><img src="magicring_thumb.png"  class="thumb" alt="魔法陣"></a>
+<table class="pg">
+  <caption></caption>
+</table>
+</blockquote>
+<h2 class="js">◆作成方法</h2>
+<blockquote>
+<p>割と作るのに苦労したので作成方法をまとめておきたいと思います．</p>
+<p>（※現状はまだ説明ページを未作成のためブログへのリンク．）</p>
+<ol>
+  <li><a href="http://triplog.hatenablog.com/entry/2014/05/13/231044#3">Webフォントの適用</a></li>
+  <li><a href="http://triplog.hatenablog.com/entry/2014/05/17/235900">画像の表示，出力</a></li>
+  <li><a href="http://triplog.hatenablog.com/entry/2014/05/18/233000">テキストの編集</a></li>
+  <li><a href="http://triplog.hatenablog.com/entry/2014/05/21/233420#3">大きなWebフォントのアップロード</a></li>
+  <li><a href="http://triplog.hatenablog.com/entry/2014/05/22/235932">Twitter連携</a></li>
+</ol>
+</blockquote>
+<h2 class="js">◆感謝</h2>
+<blockquote>
+<p>使わせて頂いたりコードを参考にしたり．</p>
+<ul>
+  <li><a href="https://github.com/Certainist/UltimateOAuth/blob/master/README-Japanese.md">UltimateOAuth</a></li>
+  <li><a href="https://github.com/sakiyamaK/OpenCVjs">OpenCVjs</a></li>
+  <li><a href="http://www.fontna.com/blog/1122/">やさしさアンチック</a></li>
+  <li><a href="http://cute-freefont.flop.jp/mikachan.html">みかちゃんフォント</a></li>
+  <li><a href="http://inatsuka.hp2.jp/extra/magicring/">MagicRing</a></li>
+  <li><a href="http://bzmm.jp/hs_gene/">ハンドサイン画像ジェネレーター</a></li>
+  <li><a href="http://hitsujiwool.net/penguin-collage/">Penguin Collage Generator</a></li>
+</ul>
+</blockquote>
+<hr>
+<p>最終更新：<?php echo date("Y.m.d.H:i:s", filemtime("index.php")); ?></p>
+</body>
+
+</html>
